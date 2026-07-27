13237c0155f13a3b16a3fa92f8a1e1bd61af36c1
Bruno <bruno.mma97@gmail.com>
2026-07-27 23:25:33 +0100

    add error handling for negative color components in linear_to_gamma

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,5 @@
 use crate::vec3::Vec3;
+use std::hint::cold_path;
 
 pub type Color = Vec3;
 
@@ -26,11 +27,11 @@ impl Color {
 }
 
 pub fn linear_to_gamma(linear_component: f64) -> f64 {
-	// if linear_component > 0.0 {
-	// 	return linear_component.sqrt();
-	// }
-	//
-	// return 0.0
-
-	linear_component.sqrt()
+	if linear_component >= 0.0 {
+		linear_component.sqrt()
+	} else {
+		cold_path();
+		println!("Warning: negative color component encountered: {}", linear_component);
+		0.0
+	}
 }
