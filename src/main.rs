a396002f682012107ddecb1c699fa18595703d29
Bruno <bruno.mma97@gmail.com>
2026-07-28 23:14:40 +0100

    move gen_world to world module

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,9 @@ use std::io::Write;
 use std::ops::RangeInclusive;
 use std::time::Instant;
 
-use crate::material::dielectric::Dielectric;
-use crate::material::lambertian::Lambertian;
-use crate::material::metal::Metal;
 use camera::Camera;
 use color::Color;
-use rand::Rng;
-use sphere::Sphere;
 use vec3::Point3;
-use world::World;
 
 mod color;
 mod vec3;
@@ -35,61 +29,8 @@ const SAMPLE_OFFSET_RANGE: RangeInclusive<f64> = -SAMPLE_OFFSET..=SAMPLE_OFFSET;
 const T_MIN: f64 = 0.001;
 const T_MAX: f64 = f64::INFINITY;
 
-fn gen_world() -> World {
-	let mut world = World::new();
-
-	let ground_material = Lambertian::new(Color::new(0.6, 0.6, 0.6));
-	world.push(Box::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
-
-	let mat_1 = Dielectric::new(1.5);
-	world.push(Box::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat_1)));
-
-	let mat_2 = Lambertian::new(Color::new(0.2, 0.2, 0.8));
-	world.push(Box::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat_2)));
-
-	let mat_3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
-	world.push(Box::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat_3)));
-
-	let mut rng =  rand::thread_rng();
-
-	for a in -11..=11 {
-		for b in -11..=11 {
-			if  a < 5 && a > -5 && b < 2 && b > -2 {
-				continue;
-			}
-
-			let center = Point3::new(
-				a as f64 + 0.9 * rng.gen_range(0.0..=1.0),
-				0.2,
-				b as f64 + 0.9 * rng.gen_range(0.0..=1.0)
-			);
-
-			let mat_float = rng.gen_range(0.0..=1.0);
-			match mat_float {
-				x if x < 0.8 => {
-					let albedo = Color::new_rand_range(0.0..=1.0);
-					let mat = Lambertian::new(albedo);
-					world.push(Box::new(Sphere::new(center, 0.2, mat)));
-				},
-				x if x < 0.95 => {
-					let albedo = Color::new_rand_range(0.5..=1.0);
-					let fuzz = rng.gen_range(0.0..=0.1);
-					let mat = Metal::new(albedo, fuzz);
-					world.push(Box::new(Sphere::new(center, 0.2, mat)));
-				},
-				_ => {
-					let mat = Dielectric::new(1.5);
-					world.push(Box::new(Sphere::new(center, 0.2, mat)));
-				},
-			};
-		}
-	}
-
-	world
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let world = gen_world();
+	let world = world::gen_world();
 
 	let cam_position = Point3::new(13.0, 2.0, -3.0);
 	let cam_look_at = Point3::new(0.0, 0.0, 0.0);
--- src/world.rs
+++ src/world.rs
@@ -1,5 +1,12 @@
+use crate::color::Color;
 use crate::hit_record::{HitRecord, Hittable};
+use crate::material::dielectric::Dielectric;
+use crate::material::lambertian::Lambertian;
+use crate::material::metal::Metal;
 use crate::ray::Ray;
+use crate::sphere::Sphere;
+use crate::vec3::Point3;
+use rand::Rng;
 
 pub type World = Vec<Box<dyn Hittable>>;
 
@@ -19,4 +26,89 @@ impl Hittable for World {
 
 		tmp_rec
 	}
+}
+
+pub fn gen_world_big() -> World {
+	let mut world = World::new();
+
+	let ground_material = Lambertian::new(Color::new(0.6, 0.6, 0.6));
+	world.push(Box::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
+
+	let mat_1 = Dielectric::new(1.5);
+	world.push(Box::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat_1)));
+
+	let mat_2 = Lambertian::new(Color::new(0.2, 0.2, 0.8));
+	world.push(Box::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat_2)));
+
+	let mat_3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
+	world.push(Box::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat_3)));
+
+	let mut rng =  rand::thread_rng();
+
+	for a in -11..=11 {
+		for b in -11..=11 {
+			if  a < 5 && a > -5 && b < 2 && b > -2 {
+				continue;
+			}
+
+			let center = Point3::new(
+				a as f64 + 0.9 * rng.gen_range(0.0..=1.0),
+				0.2,
+				b as f64 + 0.9 * rng.gen_range(0.0..=1.0)
+			);
+
+			let mat_float = rng.gen_range(0.0..=1.0);
+			match mat_float {
+				x if x < 0.8 => {
+					let albedo = Color::new_rand_range(0.0..=1.0);
+					let mat = Lambertian::new(albedo);
+					world.push(Box::new(Sphere::new(center, 0.2, mat)));
+				},
+				x if x < 0.95 => {
+					let albedo = Color::new_rand_range(0.5..=1.0);
+					let fuzz = rng.gen_range(0.0..=0.1);
+					let mat = Metal::new(albedo, fuzz);
+					world.push(Box::new(Sphere::new(center, 0.2, mat)));
+				},
+				_ => {
+					let mat = Dielectric::new(1.5);
+					world.push(Box::new(Sphere::new(center, 0.2, mat)));
+				},
+			};
+		}
+	}
+
+	world
+}
+
+pub fn gen_world() -> World {
+	let mut world = World::new();
+
+	let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+	let material_right = Lambertian::new(Color::new(0.1, 0.2, 0.5));
+	let material_left = Lambertian::new(Color::new(0.9, 0.1, 0.1));
+	let material_center = Metal::new(Color::new(0.8, 0.8, 0.8), 0.0);
+
+	world.push(Box::new(Sphere::new(
+		Point3::new(0.0, -100.5, 1.0),
+		100.0,
+		material_ground,
+	)));
+	world.push(Box::new(Sphere::new(
+		Point3::new(0.0, 0.0, 1.2),
+		0.5,
+		material_center,
+	)));
+	world.push(Box::new(Sphere::new(
+		Point3::new(-1.0, 0.0, 1.0),
+		0.5,
+		material_left,
+	)));
+	world.push(Box::new(Sphere::new(
+		Point3::new(1.0, 0.0, 1.0),
+		0.5,
+		material_right,
+	)));
+
+	world
 }
